15d966a0b8be37f639ed1f6bc400452ce99d5ac2
PedroOrtRec <pedro.ortega.dw@immune.institute>
2022-11-28 18:06:25 +0100

    Pintar pacientes. filtro pacientes por edad.

--- js/func_hospital.js
+++ js/func_hospital.js
@@ -1,26 +1,45 @@
 //DECLARACIÓN DE CONSTANTES Y VARIABLES
+const minAge = document.querySelector('#minAge');
+const maxAge = document.querySelector('#maxAge');
+const buttonAge = document.querySelector('#buttonAge');
 
-const ageForm = document.querySelector('#ageForm');
-const diagnosisForm = document.querySelector('#diagnosisForm');
-const searcherForm = document.querySelector('#searcherForm');
 const sectionPatients = document.querySelector('#patientsList');
 
-var patientsList = patients;
+let patientsList = patients;
 
 //FILTROS
 
-searcherForm.addEventListener('submit', filterSearcher)
+buttonAge.addEventListener('click', getDataAge)
 
-function filterSearcher(event) {
-    event.preventDefault();
-    console.log(event.target.searcher.value);
+function getDataAge() {
+    let min = minAge.value;
+    let max = maxAge.value;
 
+    if (max !== "" && min !== "") {
+        let filterPatient = filterByAge(patientsList, Number(min), Number(max));
+        printPatients(filterPatient, sectionPatients);
+    } else {
+        printPatients(patientsList, sectionPatients);
+    }
+
+    minAge.value = '';
+    maxAge.value = '';
+
+}
+
+function filterByAge(pPatientsList, pMin, pMax) {
+    return pPatientsList.filter(patient => patient.age >= pMin && patient.age <= pMax);
 }
 
 //PINTAR LA LISTA DE PACIENTES
 
 function printPatients(pPatientsList, pDom) {
-    pPatientsList.forEach(patient => printOnePatient(patient, pDom))
+    pDom.innerHTML = ''
+    if (pPatientsList.length !== 0) {
+        pPatientsList.forEach(patient => printOnePatient(patient, pDom))
+    } else {
+        pDom.innerHTML = `<article class="card p-2"> <h3>No hay coincidencias</h3></article>`
+    }
 }
 
 function printOnePatient(pPatient, pDom) {
@@ -28,7 +47,7 @@ function printOnePatient(pPatient, pDom) {
     let article = document.createElement('article');
     article.classList.add('card', 'mb-1');
     let h3 = document.createElement('h3');
-    h3.classList.add('bg-info');
+    h3.classList.add('bg-info', 'p-1');
     let articleDiv = document.createElement('div');
     articleDiv.classList.add('p-1');
     let p1 = document.createElement('p');
@@ -56,11 +75,6 @@ function printOnePatient(pPatient, pDom) {
 }
 
 printPatients(patientsList, sectionPatients);
-
-
-
-
-console.log(patientsList)
 
 
 
